50dbf90bd02073120979515b28c9457edef03bc6
poppabear8883 <servnx@gmail.com>
2017-11-12 22:20:05 -0500

    Added path aliases

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,16 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            Resources: path.resolve(__dirname, 'resources'),
+            Components: path.resolve(__dirname, 'resources/components'),
+            Mixins: path.resolve(__dirname, 'resources/assets/js/mixins'),
+            Views: path.resolve(__dirname, 'resources/views')
+        }
+    }
+});
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
