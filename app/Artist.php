a9bc454a7e43af80d5c5f31182ef6b8297de4718
poppabear8883 <servnx@gmail.com>
2017-11-13 19:06:55 -0500

    Added explicit keys to user relationship

--- app/Artist.php
+++ app/Artist.php
@@ -19,7 +19,7 @@ class Artist extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'owner_id', 'id');
     }
 
 }
