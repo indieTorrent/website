9fc7cefb1af44795483c97258a797907acd54642
poppabear8883 <servnx@gmail.com>
2017-11-14 16:29:16 -0500

    WIP (Trying to mock class)

--- tests/Unit/FeaturedSongsTest.php
+++ tests/Unit/FeaturedSongsTest.php
@@ -2,15 +2,49 @@
 
 namespace Tests\Unit;
 
+use App\Songs\Repositories\FeaturedSongsRepository;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\CreatesApplication;
 use Tests\TestCase;
+use Faker\Factory as Faker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FeaturedSongsTest extends TestCase
 {
+    use DatabaseMigrations, CreatesApplication;
 
-    public function test_if_type_of_featured_songs_repository()
+    protected $faker;
+
+    protected $repo;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->faker = Faker::create();
+
+        $this->repo = $this->createMock(FeaturedSongsRepository::class);
+
+        $this->seed();
+    }
+
+    public function tearDown()
+    {
+        $this->artisan('migrate:reset');
+
+        parent::tearDown();
+    }
+
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_it_responds_with_status_code_200()
+    {
+        $code = $this->get('api/featured/songs')->status();
+        $this->assertTrue(($code == 200), 'Result: ' . $code);
     }
 
     /**
@@ -18,8 +52,9 @@ class FeaturedSongsTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_artist_in_cooldown()
     {
-        $this->assertTrue(true);
+        $this->assertTrue($this->repo->isArtistInCooldown(5));
     }
+
 }
