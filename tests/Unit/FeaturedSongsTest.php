6f3c6536837c23d1a8581eed7103f3cca0260a27
poppabear8883 <servnx@gmail.com>
2017-11-14 15:00:03 -0500

    Added Featured Songs test file (no tests)

---
+++ tests/Unit/FeaturedSongsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FeaturedSongsTest extends TestCase
+{
+
+    public function test_if_type_of_featured_songs_repository()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
