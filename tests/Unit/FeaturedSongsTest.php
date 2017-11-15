f88818a9acdb48763c0a0cec610a890b946c96c0
poppabear8883 <servnx@gmail.com>
2017-11-14 19:33:13 -0500

    Fixing tests

--- tests/Unit/FeaturedSongsTest.php
+++ tests/Unit/FeaturedSongsTest.php
@@ -2,59 +2,57 @@
 
 namespace Tests\Unit;
 
-use App\Songs\Repositories\FeaturedSongsRepository;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\CreatesApplication;
+use App\Songs\Contracts\FeaturedSongsInterface;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
-use Faker\Factory as Faker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FeaturedSongsTest extends TestCase
 {
-    use DatabaseMigrations, CreatesApplication;
+    use RefreshDatabase;
 
-    protected $faker;
+    protected $table = 'featured_songs';
+
+    protected $cooldown_table = 'featured_songs_cooldown';
+
+    protected $db_connection = 'sqlite';
 
     protected $repo;
+
+    protected $db;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->faker = Faker::create();
+        $this->repo = $this->app->make(FeaturedSongsInterface::class);
+        $this->db = DB::connection($this->db_connection);
 
-        $this->repo = $this->createMock(FeaturedSongsRepository::class);
-
-        $this->seed();
     }
 
     public function tearDown()
     {
-        $this->artisan('migrate:reset');
-
         parent::tearDown();
     }
 
-
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_it_responds_with_status_code_200()
+    public function test_can_add_artist_to_cooldown()
     {
-        $code = $this->get('api/featured/songs')->status();
-        $this->assertTrue(($code == 200), 'Result: ' . $code);
+        $this->repo->addArtistToCooldown(1);
+
+        $result = $this->db->table($this->cooldown_table)
+            ->where('artist_id', 1)
+            ->first()
+            ->artist_id;
+
+        $this->assertTrue(($result == 1), 'Artist was NOT added to cooldown');
     }
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_artist_in_cooldown()
     {
-        $this->assertTrue($this->repo->isArtistInCooldown(5));
+        $this->repo->addArtistToCooldown(1);
+
+        $result = $this->repo->isArtistInCooldown(1);
+        $this->assertTrue($result, 'Artist is NOT in cooldown');
     }
 
 }
