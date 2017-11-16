4d589438d988710ab7573a67df986bdc138497ae
poppabear8883 <servnx@gmail.com>
2017-11-16 16:31:24 -0500

    Added a custom assert for checking if instance of Collection class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Collection;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function assertInstanceOfCollection($collection)
+    {
+        $this->assertInstanceOf(Collection::class, $collection);
+    }
 }
