b4c65aaf311dabc6382e22f0c5fd83aaabdc4160
poppabear8883 <servnx@gmail.com>
2017-11-16 16:30:40 -0500

    Added missing methods to interface

--- app/Songs/Contracts/FeaturedSongsInterface.php
+++ app/Songs/Contracts/FeaturedSongsInterface.php
@@ -6,11 +6,13 @@ interface FeaturedSongsInterface
 {
     public function getSongs();
     public function getSongIds();
+    public function getRank($song_id);
     public function getCooldownArtistIds();
+    public function hasExpired($expires);
     public function isArtistInCooldown($artist_id);
     public function addArtistToCooldown($artist_id);
     public function removeArtistFromCooldown($artist_id);
-    public function addRandomSong($rank = 1);
+    public function addRandomSong($rank);
     public function removeSong($song_id);
 
 }
