7195e80054da5fefaf0537e9808a9c6bebe833cf
poppabear8883 <servnx@gmail.com>
2017-11-17 15:30:45 -0500

    Added partial and boilerplate

---
+++ resources/components/SiteStats.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="site-stats" class="navbar-element-group">
+        <div class="site-stat"><strong>{{num_accounts}}</strong> customers have created accounts.</div>
+        <div class="site-stat">Fans have spent <strong>${{sum_spent}}</strong> on music.</div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                num_accounts: 0,
+                sum_spent: ''
+            }
+        },
+        methods: {
+            getAccounts() {
+                axios.get('accounts').then((response) => {
+                    // todo: count the returned collection of objects
+                    // this.num_accounts = response.data.length;
+                    this.num_accounts = 2000;
+                }).catch((error) => {
+
+                });
+            },
+            getTotalSpent() {
+                axios.get('imnotsure').then((response) => {
+                    // todo: do logic for this .. haha
+                    this.sum_spent = 6000;
+                }).catch((error) => {
+
+                });
+            },
+        },
+        created() {
+            this.getAccounts();
+            this.getTotalSpent();
+        }
+    }
+</script>
---
+++ resources/views/layouts/partials/head-meta.blade.php
@@ -0,0 +1,51 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/images/favicons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/images/favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/images/favicons/manifest.json">
+    <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#cbef4f">
+    <meta name="msapplication-TileImage" content="/images/favicons/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic'
+          rel='stylesheet'
+          type='text/css'>
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,800italic,300,600,800,400'
+          rel='stylesheet'
+          type='text/css'>
+
+    <link href='https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic,700italic'
+          rel='stylesheet'
+          type='text/css'>
+
+    <title>@yield('title')</title>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+</head>
